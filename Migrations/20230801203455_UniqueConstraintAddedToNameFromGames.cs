102dd47c8a98e4489945adce284987a23b6f4f00
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-08-01 17:39:06 -0300

    Update: Unique constraint added to column Name from Table Games.

---
+++ Migrations/20230801203455_UniqueConstraintAddedToNameFromGames.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoardcampApiCS.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueConstraintAddedToNameFromGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_Name",
+                table: "Games",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Games_Name",
+                table: "Games");
+        }
+    }
+}
